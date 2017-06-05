baa21f6ea0e73733830f2c48be898304015d66fd
PavanKrish <pavanvamsikrishna@gmail.com>
2017-06-05 12:31:53 -0400

    Update AppDelegate.swift

--- BestBuyBooks/AppDelegate.swift
+++ BestBuyBooks/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  BestBuyBooks
 //
-//  Created by Gelli,Chiklith Madhukar Siva Sai on 11/21/16.
-//  Copyright © 2016 Gelli,Chiklith Madhukar Siva Sai. All rights reserved.
+//  Created by User on 11/21/16.
+//  Copyright © 2016 Pavan,Seeram. All rights reserved.
 //
 
 import UIKit
